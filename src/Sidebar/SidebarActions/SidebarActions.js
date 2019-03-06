8f70f2d941f3483d258bca9a54bf6760ab3a8044
Daniel <oopdaniel@gmail.com>
2019-03-05 21:22:44 -0800

    layout of action bar

--- src/Sidebar/SidebarActions/SidebarActions.js
+++ src/Sidebar/SidebarActions/SidebarActions.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import Btn from '../../shared/Button/Button'
 import './SidebarActions.css';
 
 function SidebarActions () {
   return (
     <div className="sidebar-actions">
-      Actions here
+      <section className="form">
+        <input
+            className="input"
+            placeholder="Key"
+            type="text"
+          />
+
+        <input
+          className="input"
+          placeholder="Value"
+          type="text"
+        />
+      </section>
+
+      <section className="buttons">
+        <Btn>Set</Btn>
+        <Btn>Get</Btn>
+      </section>
     </div>
   );
 }
