cd5770c10c0bf958cd26cc6193eba89e0d54e588
Daniel <oopdaniel@gmail.com>
2019-03-05 17:17:04 -0800

    add btns on sidebar

--- src/Sidebar/Sidebar.js
+++ src/Sidebar/Sidebar.js
@@ -4,10 +4,10 @@ import './Sidebar.css';
 class Sidebar extends Component {
   render() {
     return (
-      <div className={`Sidebar ${this.props.customClass}`}>
-        <header className="Sidebar-options">
-          <div>Logs</div>
-          <div>Actions</div>
+      <div className={`sidebar ${this.props.customClass}`}>
+        <header className="sidebar-options flex-center">
+          <span className="sidebar-option flex-center">Logs</span>
+          <span className="sidebar-option flex-center">Actions</span>
         </header>
         <div>
           Selected: {this.props.selectedMachine === null ? 'N/A' : this.props.selectedMachine}
