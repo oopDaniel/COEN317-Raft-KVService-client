a3f61dacb9ed88bae43ca65511e89616b2a2867b
Daniel <oopdaniel@gmail.com>
2019-03-05 22:31:00 -0800

    basic log displayer

--- src/Sidebar/SidebarLogs/SidebarLogs.js
+++ src/Sidebar/SidebarLogs/SidebarLogs.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import './SidebarLogs.css';
 
-function SidebarLogs () {
+function SidebarLogs ({ logs }) {
   return (
     <div className="sidebar-logs">
-      Logs here
+      {
+        logs.length
+          ? logs.map(log => {
+              if (log.operation === 'GET') {
+                return <div className="log">Get => ({log.data.key})</div>
+              } else {
+                return <div className="log">Set => ({log.data.key}:{log.data.value})</div>
+              }
+            })
+          : <div>No available logs</div>
+      }
     </div>
   );
 }
