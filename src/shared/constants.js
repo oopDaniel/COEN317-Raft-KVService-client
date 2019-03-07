4cb56da4acb65d23a48ecd29cfddf2a179863f99
Daniel <oopdaniel@gmail.com>
2019-03-07 15:02:13 -0800

    add d3 and timeout circles

--- src/shared/constants.js
+++ src/shared/constants.js
@@ -1,2 +1,7 @@
 // TODO: Replace this with IP from AWS
 export const KNOWN_SERVER_IP = '127.0.0.1:8080'
+
+export const HEARTBEAT_INTERVAL = 4000
+export const ELECTION_TIMEOUT = 8000
+export const ELECTION_DURATION_MIN = 8000
+export const ELECTION_DURATION_MAX = 12000
