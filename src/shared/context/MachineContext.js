6da9be7d032707a2242c7e378eef0b94d83849be
Daniel <oopdaniel@gmail.com>
2019-03-07 23:13:41 -0800

    support storing position state in machineContext

--- src/shared/context/MachineContext.js
+++ src/shared/context/MachineContext.js
@@ -2,23 +2,39 @@ import React, { Component } from 'react';
 
 const MachineContext = React.createContext();
 
+// Workaround bug in setState with `position`
+const positionCache = []
 export class MachineProvider extends Component {
   state = {
     selected: null,
-    alive: {}
+    alive: {},
+    positions: {},
+    leader: null
   }
   render () {
     return (
       <MachineContext.Provider value={{
         selected: this.state.selected,
         alive: this.state.alive,
-        select: (machine) => this.setState({ selected: machine }),
+        positions: this.state.positions,
+        leader: this.state.leader,
+        select: machine => this.setState({ selected: machine }),
         unselect: () => this.setState({ selected: null }),
-        loadAlive: (alive) => this.setState({ alive }),
+        loadAlive: alive => {
+          // TODO: use leader from BE
+          const ids = Object.keys(alive)
+          const idx = ~~(Math.random() * ids.length)
+          this.setState({ alive, leader: ids[idx] })
+        },
         isAlive: id => this.state.alive[id] === true,
         toggleMachine: id => this.setState({
           alive: {...this.state.alive, [id]: !this.state.alive[id] }
-        })
+        }),
+        appendPosition: (id, newPos) => {
+          // Workaround weird bug in setState for `position`
+          positionCache.push({id, ...newPos})
+          this.setState({ positions: [...positionCache] })
+        }
       }}>
         { this.props.children }
       </MachineContext.Provider>
