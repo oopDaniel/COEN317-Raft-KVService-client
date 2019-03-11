baf2ed273036a68fb774f92d448b06c73c854d9e
Daniel <oopdaniel@gmail.com>
2019-03-11 15:45:00 -0700

    fix ack msg hopefully

--- src/Machines/MessageMap/MessageMap.js
+++ src/Machines/MessageMap/MessageMap.js
@@ -1,92 +1,107 @@
 import React, { useEffect, useContext, useState } from 'react';
-import { useObservable } from 'rxjs-hooks';
 import * as d3 from 'd3'
+import * as R from 'ramda'
 import MachineContext from '../../shared/context/MachineContext'
 // import { usePrevious } from '../../shared/utils'
-import { HEARTBEAT_INTERVAL, MSG_SINGLE_TRIP_TIME } from '../../shared/constants'
+import { MSG_SINGLE_TRIP_TIME } from '../../shared/constants'
 import './MessageMap.css';
 
 function MessageMap () {
   const {
     machineInfo: positionMap,
     leader,
-    notifySentHeartbeat,
-    receivedHeartbeat$
+    liveness,
+    leaderHeartbeat$
   } = useContext(MachineContext)
 
-  // const positionMap = useObservable(() => positions$)
+  const [circleGroups, setCircleGroups] = useState(null)
+  useEffect(() => {
+    console.log('%cLeader msg', 'color:purple;font-size:1.5em', {positionMap}, {liveness}, {leader: leader && leader.id || null}, circleGroups)
+    if (positionMap === null || liveness === null || !leader) return
+
+    let sub
+    if (!circleGroups) {
+      createMsgCircles()
+    } else {
+      sub = leaderHeartbeat$.subscribe((beat) => {
+        // console.log('beat', beat)
+        renderHeartbeatMsgChart()
+      })
+    }
+
+    return R.tryCatch(() => {
+      console.log('%cunsubscribe...sender', 'color:#000;background:#fff')
+      sub && sub.unsubscribe()
+    }, () => {})
+  }, [positionMap, liveness, leader, circleGroups])
 
   // TODO: todo
   // const prevLeader = usePrevious(leader)
 
-  // Start sending heartbeat after positionMap initialized and there's a leader
-  const [circleGroups, setCircleGroups] = useState(null)
-  const [d3Timeout, setD3Timeout] = useState(null)
-  const cleanD3Animation = () => {
-    if (d3Timeout) d3Timeout.stop()
-    setD3Timeout(null)
-  }
 
+  const [prevLeader, setPrevLeader] = useState(leader)
+  const { followerTimer$ } = useContext(MachineContext)
   useEffect(() => {
-    if (positionMap === null) return
-    if (!leader) {
-      // no leader or leader crashed => cancel animation
-      cleanD3Animation()
-    }
-    console.log('asdasd', positionMap, leader)
-    renderChart()
-    // -------------------------------------
-  }, [positionMap, leader]) // TODO: use alive
+    console.log('%ccalling ack', 'color:red;font-size:1.5em', {prevLeader: prevLeader && prevLeader.id || null}, {leader: leader && leader.id || null}, {liveness}, circleGroups)
+    if (!circleGroups || !prevLeader) return
+    const sub = followerTimer$.subscribe(replyAck)
+    return R.tryCatch(() => {
+      console.log('%cunsubscribe...ack', 'color:#000;background:#fff')
+      sub && sub.unsubscribe()
+    }, () => {})
+  }, [followerTimer$, liveness, circleGroups, prevLeader])
+
+
+  // Start sending heartbeat after positionMap initialized and there's a leader
+  // const [d3Timeout, setD3Timeout] = useState(null)
+  // const cleanD3Animation = () => {
+  //   if (d3Timeout) d3Timeout.stop()
+  //   setD3Timeout(null)
+  // }
+
+  // useEffect(() => {
+  //   if (positionMap === null || liveness === null) return
+  //   if (!leader) {
+  //     // no leader or leader crashed => cancel animation
+  //     cleanD3Animation()
+  //     return
+  //   }
+  //   console.log('%casdasd', 'color:wheat',positionMap, leader)
+  //   renderHeartbeatMsgChart()
+  //   // -------------------------------------
+  // }, [positionMap, liveness, leader]) // TODO: use alive
   // -------------------------------------
 
-  // Reply heartbeat only when received one and msg svgs are available
-  const [prevLeader, setPrevLeader] = useState(leader)
-  const followerId = useObservable(() => receivedHeartbeat$)
-  useEffect(() => {
-    if (positionMap === null || !circleGroups) return
-    replyAck()
-  }, [positionMap, circleGroups, followerId])
 
+  function createMsgCircles () {
+    const msgSvg = d3.select('svg.msg-svg')
+    const circles = msgSvg.selectAll('.circleGroups')
+      .data(R.values(R.omit([leader.id], positionMap)))
+      .enter()
+      .append('circle')
+    setCircleGroups(circles)
+    console.log('created')
+  }
 
-  function renderChart () {
-    const leaderPos = positionMap[leader]
-    if (!leaderPos) { // No leader or leader stepped down
-      if (d3Timeout) {
-        d3Timeout.stop()
-        setD3Timeout(null)
-      }
-      return
-    }
-
+  function renderHeartbeatMsgChart () {
+    console.log(' - render - ')
     const msgSvg = d3.select('svg.msg-svg')
 
     let circles = circleGroups
-    if (prevLeader !== leader) {
+    if (!circles) {
+      console.log('%cno circle', 'color:green')
       circles = msgSvg.selectAll('.circleGroups')
-        .data(Object.values(positionMap))
+        .data(R.values(R.omit([leader.id], positionMap)))
         .enter()
         .append('circle')
       setCircleGroups(circles)
-    } else {
-      if (d3Timeout) {
-        d3Timeout.stop()
-        setD3Timeout(null)
-      }
+      return false
     }
 
-    if (leader) {
-      heartbeat(leader)
-      setD3Timeout(
-        d3.interval(() => heartbeat(leader), HEARTBEAT_INTERVAL)
-      )
-    }
-
-    if (prevLeader !== leader) {
-      setPrevLeader(leader)
-    }
+    heartbeat(leader)
 
     function heartbeat (currentLeader) {
-      const { x: leaderX, y: leaderY } = positionMap[currentLeader]
+      const { x: leaderX, y: leaderY } = positionMap[currentLeader.id]
       circles
         .filter(d => d.id !== leader)
         .attr('r', 10)
@@ -102,16 +117,16 @@ function MessageMap () {
         .duration(0)
         .style('fill', 'var(--msg-ack')
         .on('end', () => {
-          notifySentHeartbeat()
           if (currentLeader) setPrevLeader(currentLeader)
         })
     }
   }
 
   function replyAck () {
-    const { x: leaderX, y: leaderY } = positionMap[leader || prevLeader]
+    // console.log(' (((- leader id', leader.id, 'prevLeader id:', prevLeader.id)
+    const { x: leaderX, y: leaderY } = positionMap[leader.id || prevLeader.id]
     circleGroups
-      .filter(d => d.id === followerId)
+      .filter(d => liveness[d.id])
       .transition()
       .duration(MSG_SINGLE_TRIP_TIME)
       .attr('cx', leaderX)
